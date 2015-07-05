d9fea0c51878b77056143db3c09adb747218a0a8
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2015-07-05 22:25:57 +0300

    Fixed PyPi release

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-install_reqs = parse_requirements('requirements.pip')
 
 def get_version(filename):
     import re
@@ -15,7 +13,10 @@ setupopts = dict(
     version=get_version('automate_wsgi/__init__.py'),
     packages=find_packages(),
 
-    install_requires=[str(ir.req) for ir in install_reqs],
+    install_requires=[
+        "automate==0.9.1",
+        "tornado==4.2"
+        ],
 
     author="Tuomas Airaksinen",
     author_email="tuomas.airaksinen@gmail.com",
