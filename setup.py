b01a39643517e6bed07b1272bdc2319df8e1470f
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2015-07-05 14:17:50 +0300

    Change extension package names to automate-something instead of automate_something

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_version(filename):
         return metadata['version']
 
 setupopts = dict(
-    name="automate_wsgi",
+    name="automate-wsgi",
     version=get_version('automate_wsgi/__init__.py'),
     packages=find_packages(),
 
