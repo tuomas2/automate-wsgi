364e314cd5f0ed8d45c4acb36057670d65bf2898
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2015-12-11 16:34:28 +0200

    Fix setup.pys and version to 0.9.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setupopts = dict(
     packages=find_packages(),
 
     install_requires=[
-        "automate>=0.9.2,<0.10",
+        "automate>=0.9.3,<0.10",
         "tornado>=4.3,<4.4"
         ],
 
